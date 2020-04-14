a36fb7fd5fd581ff935414f9d5b5db184c2ed876
Piotr Wróbel <pwrobel5@gmail.com>
2020-04-14 02:55:43 +0200

    possibility to give PSF and PDB output names when calling script

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import sys
 import readingutils
 import savingutils
 
-CORRECT_ARGC_VALUE = 2
+INPUT_ARGC_VALUE = 2
+PSF_ARGC_VALUE = 3
+PDB_ARGC_VALUE = 4
 
 
 def main():
-    if len(sys.argv) != CORRECT_ARGC_VALUE:
+    if len(sys.argv) < INPUT_ARGC_VALUE:
         input_file_name = input("Enter input file name: ")
     else:
         input_file_name = sys.argv[1]
@@ -15,11 +17,17 @@ def main():
     reader.parse_packmol_input()
     system = reader.read_xyz_data()
 
-    psf_file_name = input("Enter psf file name: ")
+    if len(sys.argv) < PSF_ARGC_VALUE:
+        psf_file_name = input("Enter psf file name: ")
+    else:
+        psf_file_name = sys.argv[2]
     psf_saver = savingutils.PSFSaver(psf_file_name, system)
     psf_saver.save_to_file()
 
-    pdb_file_name = input("Enter pdb file name: ")
+    if len(sys.argv) < PDB_ARGC_VALUE:
+        pdb_file_name = input("Enter pdb file name: ")
+    else:
+        pdb_file_name = sys.argv[3]
     pdb_saver = savingutils.PDBSaver(pdb_file_name, system)
     pdb_saver.save_to_file()
 
